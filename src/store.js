7aaf996eb098780e1a7c16d3e3ea12ff2b7a5d37
nataliaev <nataliiaevchenko@gmail.com>
2019-07-20 10:09:50 +0200

    Start of a project

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux'
+import reducer from './reducers'
+
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(reducer, enhancer)
+
+store.subscribe(() => console.log('Next state:', store.getState()))
+
+export default store
